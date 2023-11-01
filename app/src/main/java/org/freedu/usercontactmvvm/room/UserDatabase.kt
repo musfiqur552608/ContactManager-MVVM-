fd9b81d0b4c90f9f9d15b16928cd947f7d6e4440
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-11-01 18:10:52 +0600

    Room user database

---
+++ app/src/main/java/org/freedu/usercontactmvvm/room/UserDatabase.kt
@@ -0,0 +1,30 @@
+package org.freedu.usercontactmvvm.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDatabase:RoomDatabase() {
+    abstract val userDAO:UserDAO
+
+    //Singleton
+    companion object{
+        @Volatile
+        private var INSTANCE:UserDatabase?=null
+        fun getInstance(context:Context):UserDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance==null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDatabase::class.java,
+                        "user_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
